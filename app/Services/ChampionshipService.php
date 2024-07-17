a17c6731bd2ed7cbe5b471f7d6c9e2acba75eb10
Luís Gustavo Monteiro Santos <guhmonteiro10@hotmail.com>
2024-07-16 22:43:32 -0300

    Adicionando TeamRepository e ChampinshipService.

---
+++ app/Services/ChampionshipService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+use App\Domain\Team;
+use App\Domain\Game;
+use App\Repositories\TeamRepository;
+
+class ChampionshipService
+{
+    private TeamRepository $teamRepository;
+
+    public function __construct(TeamRepository $teamRepository)
+    {
+        $this->teamRepository = $teamRepository;
+    }
+
+    public function simulatedChampionship(array $teams): array
+    {
+        $this->teamRepository->clear();
+        foreach ($teams as $team) {
+            $this->teamRepository->save(new Team($team));
+        }
+
+        $teams = $this->teamRepository->findAll();
+        $rounds = [
+            'quarterfinals' => $this->simulatedRound($teams, 4),
+            'semifinals' => $this->simulatedRound($teams, 2),
+            'final' => $this->simulatedRound($teams, 1),
+        ];
+
+        return $rounds;
+    }
+
+    private function simulatedRound(array $teams, int $numGames): array
+    {
+        $games = [];
+        for ($i = 0; $i < $numGames; $i++) {
+            $team1 = array_shift($teams);
+            $team2 = array_shift($teams);
+            $score1 = rand(0, 7);
+            $score2 = rand(0, 7);
+
+            $game = new Game($team1, $team2, $score1, $score2);
+            $games[] = $game;
+
+            $winner = $game->getWinner();
+            $teams[] = $winner;
+        }
+        return $games;
+    }
+}
