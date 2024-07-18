780255cff04cf702d047ef75b7092f71677d4f77
Luís Gustavo Monteiro Santos <guhmonteiro10@hotmail.com>
2024-07-17 23:31:34 -0300

    Adicionando Css basico.

--- resources/views/championship/index.blade.php
+++ resources/views/championship/index.blade.php
@@ -2,25 +2,28 @@
 <html>
 <head>
     <title>Meu Campeonato</title>
+    <link rel="stylesheet" href="{{ asset('index.css') }}">
 </head>
 <body>
-    <h1>Insira os Times</h1>
-    @if ($errors->any())
-        <div style="color: red;">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="/simulate" method="POST">
-        @csrf
-        @for ($i = 0; $i < 8; $i++) <label for="team{{$i}}">Time {{$i + 1}}</label>
-            <input type="text" name="teams[]" id="team{{$i}}" required>
-            <br>
-        @endfor
-        <button type="submit">Simular Campeonato</button>
-    </form>
+    <div class="container">
+        <h1>Insira os Times</h1>
+        @if ($errors->any())
+            <div class="error-messages">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="/simulate" method="POST">
+            @csrf
+            @for ($i = 0; $i < 8; $i++)
+                <label for="team{{$i}}">Time {{$i + 1}}</label>
+                <input type="text" name="teams[]" id="team{{$i}}" required>
+            @endfor
+            <button type="submit">Simular Campeonato</button>
+        </form>
+    </div>
 </body>
 </html>
