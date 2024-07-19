b0163246d1c57b315a86f5d8d1114a6fb0aa1360
Luís Gustavo Monteiro Santos <guhmonteiro10@hotmail.com>
2024-07-18 21:23:41 -0300

    Melhorando o front-end.

--- resources/views/championship/results.blade.php
+++ resources/views/championship/results.blade.php
@@ -5,30 +5,42 @@
     <link rel="stylesheet" href="{{ asset('results.css') }}">
 </head>
 <body>
-    <h1>Championship</h1>
-    @foreach ($rounds as $roundName => $games)
-    <h2>{{ ucfirst($roundName) }}</h2>
-    @foreach ($games as $game)
-    <p>
-        {{ $game['host']['name'] }} ({{ $game['host']['goals'] }})
-        vs
-        ({{ $game['guest']['goals'] }}) {{ $game['guest']['name'] }}
-        @if(isset($game['penalties']))
-        <br>
-        Penalty Shootout: {{ $game['penalties']['host'] }} - {{ $game['penalties']['guest'] }}
-        @endif
-        <br>
-        Loser: {{ $game['loser']['name'] }}
-        <br>
-        <b>Winner: {{ $game['winner']['name'] }}</b>
-    </p>
-    @endforeach
-    @endforeach
-    <h2>Ranking</h2>
-    <p><b>1° Lugar:</b> {{ $ranking['1st']['name'] }}</p>
-    <p><b>2° Lugar:</b> {{ $ranking['2nd']['name'] }}</p>
-    <p><b>3° Lugar:</b> {{ $ranking['3rd']['name'] }}</p>
-    <h3>Championship Champion <br> <b>{{ $ranking['1st']['name'] }}</b></h3>
-    <a href="/">Voltar</a>
+    <div class="container">
+        <h1>Championship</h1>
+        <div class="rounds container">
+            @foreach ($rounds as $roundName => $games)
+            <div class="rounds">
+                <h2>{{ ucfirst($roundName) }}</h2>
+                @foreach ($games as $game)
+                <p>
+                    <b>{{ $game['host']['name'] }} ({{ $game['host']['goals'] }})
+                        vs
+                        ({{ $game['guest']['goals'] }}) {{ $game['guest']['name'] }}</b>
+                    @if(isset($game['penalties']))
+                    <br>
+                    <b class="penalties">Penalty Shootout: {{ $game['penalties']['host'] }} - {{ $game['penalties']['guest'] }}</b>
+                    @endif
+                    <br>
+                    <b class="loser">Loser: {{ $game['loser']['name'] }}</b>
+                    <br>
+                    <b class="winner">Winner: {{ $game['winner']['name'] }}</b>
+                </p>
+                @endforeach
+            </div>
+            @endforeach
+            <h3>Championship Champion <br> <b>{{ $ranking['1st']['name'] }}</b></h3>
+        </div>
+        <h2>Ranking</h2>
+        <p><b>1° Lugar: {{ $ranking['1st']['name'] }}</b></p>
+        <p><b>2° Lugar: {{ $ranking['2nd']['name'] }}</b></p>
+        <p><b>3° Lugar: {{ $ranking['3rd']['name'] }}</b></p>
+        <div class="buttons">
+            <p>
+                <a href="/">Back</a>
+                <a href="/simulate">Simulate Again</a>
+                <a href="/historic">Historic</a>
+            </p>
+        </div>
+    </div>
 </body>
 </html>
